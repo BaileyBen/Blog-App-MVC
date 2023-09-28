dd3047289a7a446d186a0ce6d8a80a20e62d2a8f
BaileyBen <l3enl3ailey@hotmail.com>
2023-09-28 15:43:36 +1000

    Finished implemented CRUD for Admin Tags Controller and View Models

--- Blog.Web/Controllers/AdminTagsController.cs
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -32,16 +32,78 @@ namespace Blog.Web.Controllers
             blogDbContext.Tags.Add(tag);
             blogDbContext.SaveChanges();
 
-            return View("Add");
+            return RedirectToAction("List");
         }
 
         [HttpGet]
-        public IActionResult List() 
+        [ActionName("List")]
+        public IActionResult List()
         {
 
             var tags = blogDbContext.Tags.ToList();
 
             return View(tags);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            var tag = blogDbContext.Tags.FirstOrDefault(x => x.Id == id);
+
+            if (tag != null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName,
+                };
+
+                return View(editTagRequest);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName,
+            };
+
+            var existingTag = blogDbContext.Tags.Find(tag.Id);
+
+            if (existingTag != null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+
+                blogDbContext.SaveChanges();
+
+                return RedirectToAction("Edit", new { id = editTagRequest.Id });
+            };
+
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(EditTagRequest editTagRequest)
+        {
+            var tag = blogDbContext.Tags.Find(editTagRequest.Id);
+
+            if (tag != null)
+            {
+                blogDbContext.Tags.Remove(tag);
+                blogDbContext.SaveChanges();
+
+                return RedirectToAction("List");
+            };
+
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
         }
     }
 }
